796150e97a67ec7ebc09f47bdd6631affbb3df9b
donald-macritchie <>
2023-11-29 14:25:49 +0000

    removeed secret key and set debug

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -26,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7g8&4ae5z%ytptzd6x_%hax@qzbu+x$y$i)9zy0mc80i&!4#3#'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['8000-donald-macritchie-boutiq-6juyhuae8a.us2.codeanyapp.com',
                  'boutique-ado-dm-810fdc1539b6.herokuapp.com']
